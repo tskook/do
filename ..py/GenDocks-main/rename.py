5c32ce722f67c3970d6db384672607b851ae8bfd
tskook <90546207+tskook@users.noreply.github.com>
2021-09-12 19:31:18 +0300

    big

--- ..py/GenDocks-main/rename.py
+++ ..py/GenDocks-main/rename.py
@@ -1,7 +1,7 @@
 import os
-path = r'C:\Users\u9133908\Documents\GitHub\do\..py\GenDocks-main\redpanda'
+path = r'C:\Users\u9133908\Documents\GitHub\do\..py\GenDocks-main\not'
 files = os.listdir(path)
 
 
 for index, file in enumerate(files):
-    os.rename(os.path.join(path, file), os.path.join(path, str(index).join(['redpanda.', '.jpg'])))
+    os.rename(os.path.join(path, file), os.path.join(path, str(index).join(['not.', '.jpg'])))
