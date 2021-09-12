db5e3881f08b917d66c41273c9aaa7bc90794d0c
tskook <90546207+tskook@users.noreply.github.com>
2021-09-12 19:02:36 +0300

    Update ai_v0.py

--- ..py/GenDocks-main/ai_v0.py
+++ ..py/GenDocks-main/ai_v0.py
@@ -1,48 +1,7 @@
-import os
-import caer
-import canaro
-import numpy as np
-import cv2 as cv
-import gc
 
+from imageai.Classification.Custom import ClassificationModelTrainer
 
-IMG_SIZE = (80, 80)
-channels = 1
-an_path = r'C:\Users\u9133908\Documents\GitHub\do\..py\GenDocks-main\aa'
-
-an_dict = {}
-for ani in os.listdir(an_path):
-    an_dict[ani] = len(os.listdir(os.path.join(an_path, ani)))
-
-an_dict = caer.sort_dict(an_dict, descending=True)
-print(an_dict)
-
-rp = []
-for an in an_dict:
-    rp.append(an[0])
-print(rp)
-
-
-train = caer.preprocess_from_dir(an_path, rp, channels=channels, IMG_SIZE=IMG_SIZE, isShuffle=True)
-
-import matplotlib.pyplot as plt
-plt.figure(figsize=(30,30))
-plt.imshow(train[0][0], cmap='gray')
-plt.show()
-
-featset, labels = caer.sep_train(train, IMG_SIZE=IMG_SIZE)
-# Normalize featset ==> (0,1)
-from tensorflow.keras.utils import to_categorical
-featset = caer.normalize(featset)
-labels = to_categorical(labels, len(rp))
-
-x_train, x_val, y_train, y_val = caer.train_val_split(featset, labels, val_ratio=.2)
-
-BATCH_SIZE = 32
-EPOCHS = 10
-
-datagen = canaro.generators.imageDataGenerator()
-train_gen = datagen.flow(x_train, y_train, batch_size=BATCH_SIZE)
-
-model = canaro.models.createDefaultModel(IMG_SIZE=IMG_SIZE, channels=channels, output_dims=len(rp), loss='binary_crossentropy', decay=1e-6, learning_rate=0.001, momentum=0.9, nesterov=True)
-model.summary()
+model_trainer = ClassificationModelTrainer()
+model_trainer.setModelTypeAsResNet50()
+model_trainer.setDataDirectory(r"C:\Users\u9133908\Documents\GitHub\do\..py\GenDocks-main\aa\redpanda")
+model_trainer.trainModel(num_objects=10, num_experiments=200, enhance_data=True, batch_size=32, show_network_summary=True)
