b4a49daa60c7f3082e6ac42ac9f6081a82bc16dc
tskook <90546207+tskook@users.noreply.github.com>
2021-09-13 20:34:06 +0300

    up

--- ..py/GenDocks-main/ai_v0.py
+++ ..py/GenDocks-main/ai_v0.py
@@ -1,119 +1,89 @@
-import cv2
+import matplotlib.pyplot as plt
 import numpy as np
 import os
-from random import shuffle 
-from tqdm import tqdm
-import matplotlib.pyplot as plt
-import tflearn
-from tflearn.layers.conv import conv_2d, max_pool_2d
-from tflearn.layers.core import input_data, dropout, fully_connected
-from tflearn.layers.estimator import regression
+import PIL
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+batch_size = 32
+img_height = 180
+img_width = 180
+
+img_dir = r'C:\Users\u9133908\Documents\GitHub\do\..py\GenDocks-main\train'
+
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  img_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  img_dir,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+class_names = train_ds.class_names
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+normalization_layer = layers.experimental.preprocessing.Rescaling(1./255)
+
+num_classes = 5
+
+model = Sequential([
+  layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+  layers.Conv2D(16, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(num_classes)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
 
 
-TRAIN_DIR = r'C:\Users\u9133908\Documents\GitHub\do\..py\GenDocks-main\train'
-TEST_DIR = r'C:\Users\u9133908\Documents\GitHub\do\..py\GenDocks-main\test'
-IMG_SIZE = 60
-LR = 1e-3
-
-MODEL_NAME = 'redpandadetection-{}-{}.model'.format(LR, '2conv-basic')
-
-def label_img(img):
-    word_label = img.split('.')[-3]
-    if word_label == 'redpanda': return [1,0]
-
-    elif word_label == 'not': return [0,1]
-
-def create_train_data():
-    training_data = []
-    for img in tqdm(os.listdir(TRAIN_DIR)):
-        label = label_img(img)
-        path = os.path.join(TRAIN_DIR,img)
-        img = cv2.imread(path,cv2.IMREAD_GRAYSCALE)
-        img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))
-        training_data.append([np.array(img),np.array(label)])
-    shuffle(training_data)
-    np.save('train_data.npy', training_data)
-    return training_data
-
-def process_test_data():
-    testing_data = []
-    for img in tqdm(os.listdir(TEST_DIR)):
-        path = os.path.join(TEST_DIR,img)
-        img_num = img.split('.')[0]
-        img = cv2.imread(path,cv2.IMREAD_GRAYSCALE)
-        img = cv2.resize(img, (IMG_SIZE,IMG_SIZE))
-        testing_data.append([np.array(img), img_num])
-        
-    shuffle(testing_data)
-    np.save('test_data.npy', testing_data)
-    return testing_data
-
-train_data = create_train_data()
-
-#train_data = np.load('train_data.npy')
-
-convnet = input_data(shape=[None, IMG_SIZE, IMG_SIZE, 1], name='input')
-
-convnet = conv_2d(convnet, 32, 5, activation='relu')
-convnet = max_pool_2d(convnet, 5)
-
-convnet = conv_2d(convnet, 64, 5, activation='relu')
-convnet = max_pool_2d(convnet, 5)
-
-convnet = fully_connected(convnet, 1024, activation='relu')
-convnet = dropout(convnet, 0.8)
-
-convnet = fully_connected(convnet, 2, activation='softmax')
-convnet = regression(convnet, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
-
-model = tflearn.DNN(convnet, tensorboard_dir='log')
-
-if os.path.exists('{}.meta'.format(MODEL_NAME)):
-    model.load(MODEL_NAME)
-    print('model loaded!')
-
-train = train_data[:-500]
-test = train_data[-500:]
-
-X = np.array([i[0] for i in train]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
-Y = [i[1] for i in train]
-
-test_x = np.array([i[0] for i in test]).reshape(-1,IMG_SIZE,IMG_SIZE,1)
-test_y = [i[1] for i in test]
-
-model.fit({'input': X}, {'targets': Y}, n_epoch=3, validation_set=({'input': test_x}, {'targets': test_y}), 
-    snapshot_step=500, show_metric=True, run_id=MODEL_NAME)
-
-model.save(MODEL_NAME)
+data_augmentation = keras.Sequential(
+  [
+    layers.experimental.preprocessing.RandomFlip("horizontal", 
+                                                 input_shape=(img_height, 
+                                                              img_width,
+                                                              3)),
+    layers.experimental.preprocessing.RandomRotation(0.1),
+    layers.experimental.preprocessing.RandomZoom(0.1),
+  ]
+)
 
 
-test_data = process_test_data()
-#test_data = np.load('test_data.npy')
+test_path = r'C:\Users\u9133908\Documents\GitHub\do\..py\GenDocks-main\test\2.jpeg'
 
-fig=plt.figure()
+img = keras.preprocessing.image.load_img(
+    test_path, target_size=(img_height, img_width)
+)
+img_array = keras.preprocessing.image.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
 
-for num,data in enumerate(test_data[:12]):
-    
-    img_num = data[1]
-    img_data = data[0]
-    
-    y = fig.add_subplot(3,4,num+1)
-    orig = img_data
-    data = img_data.reshape(IMG_SIZE,IMG_SIZE,1)
-    model_out = model.predict([data])
-
-    print(model_out)
-    
-    if np.argmax(model_out) == 1: str_label=f'{num}not'
-    else: str_label=f'{num}redpanda'
-        
-    y.imshow(orig,cmap='gray')
-    plt.title(str_label)
-    y.axes.get_xaxis().set_visible(False)
-    y.axes.get_yaxis().set_visible(False)
-plt.show()
-
-
-
-
-
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+print(score)
+print(class_names)
+print(np.argmax(score))
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
